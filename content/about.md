e350bf072641c3c69a1253a6e68e3e56defaed13
GuillaumePressiat <guillaumepressiat@MacBook-Pro-de-Guillaume.local>
2018-03-03 15:33:44 +0100

    updates pour netlify

--- content/about.md
+++ content/about.md
@@ -4,4 +4,7 @@ date: 2018-03-05
 draft: false
 ---
 
-À propos !
+Bienvenue, 
+
+Ce site rassemble des travaux faits avec R et pmeasyr dans le domaine de l'Information Médicale, à l'AP-HP.
+
--- setup.R
+++ setup.R
@@ -2,13 +2,9 @@ library(blogdown)
 
 # getwd()
 new_site('')
-new_site('test2')
 install_theme('calintat/minimal')
 
 serve_site()
 build_site()
 
-
-new_post('Coucou')
-
-new_content('Coucou 2')
+zip( 'public.zip', files = 'public')
